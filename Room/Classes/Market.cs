a0520be45855fd8075d7b90f03a78de8fe394967
eaheil <129311664+eaheil@users.noreply.github.com>
2024-11-14 11:54:04 -0700

    Market is now a Commons IRoom

--- Room/Classes/Market.cs
+++ Room/Classes/Market.cs
@@ -1,46 +1,26 @@
-public class Market : IRoom {
-    public int number;
-    public int floor;
-    public int capacity;
-    public string name;
-    public string type;
-    
+public class Market : Commons, IRoom {
+    public List<IEquipment> equipments;
+    public List<Department> departments;
+    public Building buildings;
+    public Floor floors;
+
     public void AddEquipment(IEquipment equipment) {
         equipments.Add(equipment);
     }
-    
+
     public void RemoveEquipment(IEquipment equipment) {
         equipments.Remove(equipment);
     }
+
     public void AddRoom(IRoom room) {
         rooms.Add(room);
     }
-    
+
     public void RemoveRoom(IRoom room) {
         rooms.Remove(room);
     }
-    
+
     public void AddDepartment(Department department) {
         departments.Add(department);
-    }
-    
-    public void RemoveDepartment(Department department) {
-        departments.Remove(department);
-    }
-    
-    public void AddBuilding(IBuilding building) {
-        buildings.Add(building);
-    }
-    
-    public void RemoveBuilding(IBuilding building) {
-        buildings.Remove(building);
-    }
-    
-    public void AddFloor(IFloor floor) {
-        floors.Add(floor);
-    }
-    
-    public void RemoveFloor(IFloor floor) {
-        floors.Remove(floor);
     }
 }
