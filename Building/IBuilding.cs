6b39061137d25bfb913fe5900f2f38f8eca775e4
eaheil <129311664+eaheil@users.noreply.github.com>
2024-11-14 11:22:18 -0700

    IBuilding Methods

--- Building/IBuilding.cs
+++ Building/IBuilding.cs
@@ -1,1 +1,18 @@
-public interface IBuilding {}
+public interface IBuilding {
+    string name;
+    string address;
+
+    void AddRoom(IRoom room);
+    void RemoveRoom(IRoom room);
+    void AddDepartment(Department department);
+    void RemoveDepartment(Department department);
+    void AddEmployee(IEmployee employee);
+    void RemoveEmployee(IEmployee employee);
+    void AddEquipment(IEquipment equipment);
+    void RemoveEquipment(IEquipment equipment);
+    void AddBuilding(IBuilding building);
+    void RemoveBuilding(IBuilding building);
+    void AddFloor(IFloor floor);
+    void RemoveFloor(IFloor floor);
+    
+}
