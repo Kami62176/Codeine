e9f51dba9d7b82d01a56f33508bce60aa48c3ea5
dvanriette <123405372+dvanriette@users.noreply.github.com>
2024-11-21 11:34:21 -0700

    more stuff

--- Equipment/Item.cs
+++ Equipment/Item.cs
@@ -1,6 +1,9 @@
 public abstract class Item : IEquipment
 {
-    public Item(string name) : base(name){
-        
+    public float cost;
+    public int quantity;
+    public Item(string name, float cost, int quantity):base(name){
+        this.cost = cost;
+        this.quantity = quantity;
     }
 }
---
+++ Equipment/Snack.cs
@@ -0,0 +1,6 @@
+public class Snack:Item{
+
+    public Snack(string name, int quantity, float cost):base(name,cost,quantity){
+        
+    }
+}
